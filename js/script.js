c05970e764753c05fe4bf15f63ca3ad2d1c0c90c
Serg L <example@gmail.com>
2020-10-24 13:26:08 +0300

    домашнее задание

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use strict";
-let number = 4.6;
+/*let number = 4.6;
 console.log(4/0);
 
 const obj = {
@@ -8,4 +8,20 @@ const obj = {
     isMarried: false
 };
 console.log(obj["name"]);
-document.write(answers);
+document.write(answers); */
+const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?' , '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+const a=prompt('Один из последних просмотренных фильмов?', ''),
+      b=prompt('На сколько оцените его?', ''),
+      c=prompt('Один из последних просмотренных фильмов?', ''),
+      d=prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
